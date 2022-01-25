a565598a5aa038709ca1f9e9578b593f07460276
NagyLP <nagy.peter@pr.hu>
2022-01-25 15:32:47 +0100

    day02: MovieService

--- src/main/java/day02/Movie.java
+++ src/main/java/day02/Movie.java
@@ -5,23 +5,21 @@ import java.util.List;
 public class Movie {
 
     private String title;
+    private int length;
     private List<String> actor;
 
-    public Movie(String title, List<String> actor) {
+    public Movie(String title, int length, List<String> actor) {
         this.title = title;
-        this.actor = actor;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setActor(List<String> actor) {
+        this.length = length;
         this.actor = actor;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public int getLength() {
+        return length;
     }
 
     public List<String> getActor() {
--- src/main/java/day02/MovieService.java
+++ src/main/java/day02/MovieService.java
@@ -6,5 +6,20 @@ public class MovieService {
 
     private List<Movie> movies;
 
-    public void
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+
+    public List<Movie> containsActor(String actor) {
+        return movies.stream()
+                .filter(movie -> movie.getActor().contains(actor))
+                .toList();
+    }
+
+    public int getLongestMovies(String movie) {
+        return movies.stream()
+                .mapToInt(m -> m.getLength())
+                .max()
+                .orElseThrow(() -> new IllegalStateException("Empty list"));
+    }
 }
