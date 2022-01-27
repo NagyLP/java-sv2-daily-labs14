7ca965cb037692513a8d1eeac70cdece8644a2a6
NagyLP <nagy.peter@pr.hu>
2022-01-28 00:17:35 +0100

    day04: PairFinder (R)

--- src/main/java/day04/PairFinder.java
+++ src/main/java/day04/PairFinder.java
@@ -14,7 +14,6 @@ public class PairFinder {
         int[] result = Arrays.copyOf(arr, arr.length);
         Arrays.sort(result);
 
-
         int count = 0;
         for (int i = 1; i < result.length; i++) {
             if (result[i] == result[i - 1]) {
@@ -37,21 +36,23 @@ public class PairFinder {
 
 
     // REKURZIÓ!!!
-//    public int findPairsRecurcion(List<Integer> arr) {
-//        if (arr.size() > 0) {
-//            int count = switch (arr.get(0)) {
-//                case arr.get(0) / arr.get(1) == arr.get(0) ? 1 : 0;
-//                default -> 0;
-//            };
-//            return count + findPairs(arr.remove(0));
-//        }
-//        return 0;
-//    }
+    // Nem teljesen jó: sok, hosszú...
+    public int findPairsRecurcion(int[] arr) {
+        if (arr.length > 0) {
+            int count = switch (arr[0] / arr[1]) {
+                case 1 -> 1;
+                default -> 0;
+            };
+            return count + findPairs(arr);
+        }
+        return 0;
+    }
 
 
     public static void main(String[] args) {
         PairFinder pf = new PairFinder();
         System.out.println(pf.findPairs(new int[]{1, 3, 5, 5, 4, 4, 4, 6}));
         System.out.println(pf.findPairsInList(Collections.unmodifiableList(new ArrayList<>(Arrays.asList(1, 3, 5, 5, 4, 4, 4, 6)))));
+        System.out.println(pf.findPairsRecurcion(new int[]{1, 3, 5, 5, 4, 4, 4, 6}));
     }
 }
